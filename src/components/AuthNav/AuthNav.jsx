04a04a58683fb2be857341206eba4080365b4bb1
Olga007 <o.piankova@ukr.net>
2023-06-01 23:35:35 +0300

    navlink css

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -5,8 +5,8 @@ import css from "./AuthNav.module.css";
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Log In</NavLink>
+      <NavLink to="/register" className={css.link}>Register</NavLink>
+      <NavLink to="/login" className={css.link}>Log In</NavLink>
     </div>
   );
 };
